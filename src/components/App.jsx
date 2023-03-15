5d76d3c7dd7da14ff9703db017cb0fb8dd353419
iamblashch <bblashchuk08@gmail.com>
2023-03-15 17:58:58 +0200

    add_header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
+import { Header } from './Header/Header';
+import { BrowserRouter } from 'react-router-dom';
+
 export const App = () => {
   return (
-   <>
-    
-   </>
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
   );
 };
