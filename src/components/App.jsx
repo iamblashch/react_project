33c4557b88d4d19536ce6716e19bbfbbb446d6c4
iamblashch <bblashchuk08@gmail.com>
2023-03-16 10:19:18 +0200

    conflict_update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
-import { Header } from './Header/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { Header } from './Header/Header';
 import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
 import { LoginPage } from 'pages/LoginPage/LoginPage';
 
-import Navigation from "./Navigation/Navigation";
+import Navigation from './Navigation/Navigation';
 
 export const App = () => {
   return (
-<<<<<<< Updated upstream
-   <>
     <BrowserRouter basename="/react_project">
-     <Header />
+      <Header />
       <RegistrationPage />
       <LoginPage />
-    <Navigation />
-=======
-    <BrowserRouter>
-      {/* <Header /> */}
-      <RegistrationPage />
-      {/* <LoginPage /> */}
->>>>>>> Stashed changes
+      <Navigation />
     </BrowserRouter>
-   </>
+  );
+};
