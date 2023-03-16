592fad075dad7595e312fcb9029dd94cfcc62973
Ihor-Samburenko <samburenko.igor@gmail.com>
2023-03-16 20:27:32 +0200

    currency

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 // import { Header } from './Header/Header';
-import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
+// import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
+import { Currency } from './Currency/Currency';
 // import { LoginPage } from 'pages/LoginPage/LoginPage';
 
 export const App = () => {
   return (
     <BrowserRouter basename="/react_project">
       {/* <Header /> */}
-      <RegistrationPage />
+      {/* <RegistrationPage /> */}
+      <Currency />
     </BrowserRouter>
   );
 };
