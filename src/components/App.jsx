0b5a9aacf0589a65b69f4859136f7cc15a32af39
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-03-16 16:31:47 +0200

    loginPage,auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import { Header } from './Header/Header';
 import { BrowserRouter } from 'react-router-dom';
 import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
-
+import { PersistGate } from 'redux-persist/integration/react';
 import { LoginPage } from 'pages/LoginPage/LoginPage';
-
+import { Provider } from 'react-redux';
+import { store, persistor } from '../redux/store';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <RegistrationPage />
-      <LoginPage />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Header />
+          <RegistrationPage />
+          <LoginPage />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   );
 };
