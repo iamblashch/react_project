1cfc75685d97eca8acb8dd96b578c3742d9286a0
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-03-17 14:02:48 +0200

    AddRegistration/Login-Validation

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,20 +1,21 @@
-import { useDispatch } from 'react-redux';
+
 import TextField from '../../shared/Component/textField/textField';
 import fields from '../../shared/Component/textField/fields';
 import Style from '../../components/RegistrationForm/RegistrationForm.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
 import { ButtonCurrent } from '../../shared/Component/ButtonFrom/ButtonFrom';
 import { ButtonConversion } from '../../shared/Component/ButtonFrom/ButtonFrom';
-
+import { NavLink } from "react-router-dom";
 import { Logo } from '../../shared/Component/LogoForm/logoForm';
 import { MdEmail } from 'react-icons/md';
 import { AiFillLock } from 'react-icons/ai';
 import { login } from '../../redux/auth/auth-operations';
-
+import { getError } from '../../redux/auth/auth-selectors';
 const LoginForm = () => {
   const [stateLogin, setSateLogin] = useState({ email: '', password: '' })
   const dispatch = useDispatch();
-  
+    const error = useSelector(getError);
 const hendleChange = e => {
   const { name, value } = e.target;
     setSateLogin(prevState => ({
@@ -26,11 +27,11 @@ const hendleChange = e => {
    e.preventDefault();
    dispatch(login(stateLogin));
    setSateLogin({email:'', password:''})
-
   }
   return (
     <>
       <Logo />
+       {error && <p className={Style.errorText}>{error}</p>}
       <form className={Style.RegisterForm} onSubmit={hendleSubmit}>
         <TextField
           icon={<MdEmail className={Style.Icons} />}
@@ -46,7 +47,7 @@ const hendleChange = e => {
         />
         <ButtonCurrent name={'LOG IN'} />
       </form>
-      <ButtonConversion name={'REGISTER'} />
+      <NavLink to="Register"><ButtonConversion name={'REGISTER'} /></NavLink>
     </>
   );
 };
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -17,15 +17,17 @@ import { MdEmail } from 'react-icons/md';
 import { AiFillLock } from 'react-icons/ai';
 import { FaUser } from 'react-icons/fa';
 ///
-import {getError} from "../../redux/auth/auth-selectors"
+import { getError } from '../../redux/auth/auth-selectors';
+import { NavLink } from "react-router-dom";
 
 const RegistrationForm = () => {
-
   const [stateRegistr, setStateRegistr] = useState(initialState);
-  const [dublicat, setDublicat] = useState(false)
+  const [dublicat, setDublicat] = useState(false);
+  const [passwordLength, setPasswordLength] = useState(false);
+
   const dispatch = useDispatch();
-  const error = useSelector(getError)
-  
+  const error = useSelector(getError);
+
   const hendleChange = e => {
     const { name, value } = e.target;
     setStateRegistr(prevState => ({
@@ -35,31 +37,50 @@ const RegistrationForm = () => {
   };
 
   const hendlSubmit = e => {
+    setPasswordLength(false);
+    setDublicat(false);
     e.preventDefault();
     if (stateRegistr.password !== stateRegistr.confirmPassword) {
-    setDublicat(prevState => !prevState)
-    return
+      setDublicat(prevState => !prevState);
+      return;
+    } else if (stateRegistr.password.length < 5) {
+      setPasswordLength(prevState => !prevState);
+      return;
+    } else {
+      const payload = {
+        username: stateRegistr.username,
+        email: stateRegistr.email,
+        password: stateRegistr.password,
+      };
+      dispatch(register(payload));
+      setStateRegistr(initialState);
     }
-    const payload = {
-      username: stateRegistr.username,
-      email: stateRegistr.email,
-      password: stateRegistr.password,
-    };
-    dispatch(register(payload));
   };
 
   return (
     <>
       <Logo />
+      {passwordLength && (
+        <p className={Style.errorText}>Password must be 5 or more characters</p>
+      )}
       {error && <p className={Style.errorText}>{error}</p>}
       {dublicat && <p className={Style.errorText}>Enter the same password</p>}
       <form className={Style.RegisterForm} onSubmit={hendlSubmit}>
-        <TextField
-          icon={<MdEmail className={Style.Icons} />}
-          value={stateRegistr.email}
-          onChange={hendleChange}
-          {...fields.email}
-        />
+        {!error ? (
+          <TextField
+            icon={<MdEmail className={Style.Icons} />}
+            value={stateRegistr.email}
+            onChange={hendleChange}
+            {...fields.email}
+          />
+        ) : (
+          <TextField
+            icon={<MdEmail className={Style.Icons} />}
+            value={stateRegistr.email}
+            onChange={hendleChange}
+            {...fields.email}
+          />
+        )}
         <TextField
           icon={<AiFillLock className={Style.Icons} />}
           value={stateRegistr.password}
@@ -81,7 +102,7 @@ const RegistrationForm = () => {
         />
         <ButtonCurrent name={`REGISTER`} />
       </form>
-      <ButtonConversion name={`LOG IN`} />
+      <NavLink to="Login"><ButtonConversion name={`LOG IN`} /></NavLink>
     </>
   );
 };
