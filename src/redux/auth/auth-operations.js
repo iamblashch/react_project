00edef6b2c9029fb100c5f6c2ba6f66cba98fa93
iamblashch <bblashchuk08@gmail.com>
2023-03-21 16:54:55 +0200

    upate_link

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import * as api from '../../shared/Api/auth';
+import * as api from '../../shared/api/auth';
 
 
 export const register = createAsyncThunk(
